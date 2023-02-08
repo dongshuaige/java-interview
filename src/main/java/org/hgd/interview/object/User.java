fada4cd5bc1fbd35e693c2f59907f48b32a4e286
1465158178@qq.com <hgd19980114@>
2023-02-08 11:15:26 +0800

    面试题

---
+++ src/main/java/org/hgd/interview/object/User.java
@@ -0,0 +1,48 @@
+package org.hgd.interview.object;
+
+import java.util.Objects;
+
+/**
+ * @description:
+ * @author：hgd
+ * @date: 2023-02-07
+ */
+public class User {
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return Objects.equals(name, user.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        // return name.hashCode();
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/hgd/interview/object/ZhouYuList.java
@@ -0,0 +1,10 @@
+package org.hgd.interview.object;
+
+/**
+ * @description:
+ * @author：hgd
+ * @date: 2023-02-07
+ */
+public class ZhouYuList<E extends Number> {
+    public void add(E e){}
+}
